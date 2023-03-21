cba780e3b227a3fa5787902f5051f660bb3ce0d8
Aryan <aryan.saxena094@gmail.com>
2023-03-21 03:50:40 -0400

    final commit

--- game.js
+++ game.js
@@ -1,18 +1,63 @@
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 var userClickedPattern = [];
+var level = 0;
+var started = false;
 
+$(document).keypress(function(event){
+    if(!started){
+        $("#level-title").text("Level "+level);
+        nextSequence();
+        started = true;
+    }
+})
 $(".btn").click(function(){
-    var userChosenColour = $(".btn").attr("id");
-    playSound(userChosenColour);
+    var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
 });
 
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        
+        console.log("success");
+    
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 1000);
+        }
+
+    }
+    else {
+        console.log("wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+
+}
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
 function nextSequence(){
-    var randomNumber =  Math.floor((Math.random()*3));
-    randomChosenColour = buttonColours[randomNumber];
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level "+level);
+    var randomNumber =  Math.floor((Math.random()*4));
+    var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
 }
 
@@ -21,8 +66,8 @@ function playSound(name){
 }
 
 function animatePress(currentColor){
-    $("#"+currentColor).addClass('pressed');
+    $("#"+currentColor).addClass("pressed");
     setTimeout(function(){
-        $("#"+currentColor).removeClass('pressed');
+        $("#"+currentColor).removeClass("pressed");
     }, 100);
 }
